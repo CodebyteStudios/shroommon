693bc0472bca8782d1b719847620d86c2f60fa7d
Jonathan Graham <theflametrooper@gmail.com>
2016-10-03 19:30:10 +0000

    Basic Sprite Movement with WASD

--- game.js
+++ game.js
@@ -1,7 +1,52 @@
-/*global PIXI*/
-
-var renderer = new PIXI.WebGLRenderer(window.innerWidth, window.innerHeight, { backgroundColor : 0x0000000 });
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 
-document.body.appendChild(renderer.view);
+var bunny = new PIXI.Sprite(PIXI.Texture.fromImage('sprite.png'));
+    bunny.anchor.x = 0;
+	bunny.anchor.y = 0;
+    bunny.position.x = window.innerWidth / 2;
+	bunny.position.y = window.innerHeight / 2;
+	
+var bunnyHeight = renderer.height / 20;
+var bunnyWidth = renderer.width / 20;
+
+stage.addChild(bunny);
+
+animate();
+
+function animate() {
+    requestAnimationFrame(animate);
+	renderer.render(stage);
+}
+
+document.addEventListener('keydown', onKeyDown);
+
+function onKeyDown(key) {
+
+  if (key.keyCode === 87) {
+      if (bunny.position.y != 0) {
+          bunny.position.y -= bunnyHeight;
+      }
+  }
+
+  if (key.keyCode === 83) {
+      if (bunny.position.y != renderer.height - bunnyHeight) {
+          bunny.position.y += bunnyHeight;
+      }
+  }
+
+  if (key.keyCode === 65) {
+      if (bunny.position.x != 0) {
+          bunny.position.x -= bunnyWidth;
+      }
+  }
+
+  if (key.keyCode === 68) {
+      if (bunny.position.x != renderer.width - bunnyWidth) {
+          bunny.position.x += bunnyWidth;
+      }
+  }
+  
+}
