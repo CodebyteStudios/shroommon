3694217b557ab27beae77b3c2678d8a482f6f3e0
Jonathan Graham <theflametrooper@gmail.com>
2016-10-04 18:12:23 +0000

    Added platform and spacebar for jumping

--- game.js
+++ game.js
@@ -1,52 +1,51 @@
-var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.view);
-
-var stage = new PIXI.Container();
-
-var bunny = new PIXI.Sprite(PIXI.Texture.fromImage('sprite.png'));
-	bunny.anchor.x = 0;
-	bunny.anchor.y = 0;
-	bunny.position.x = window.innerWidth / 2;
-	bunny.position.y = window.innerHeight / 2;
+(function() {
+	var renderer = PIXI.autoDetectRenderer(160, 144);
+	document.body.appendChild(renderer.view);
 	
-var bunnyHeight = renderer.height / 20;
-var bunnyWidth = renderer.width / 20;
+	var stage = new PIXI.Container();
+	
+	var boxWidth = renderer.width / 20;
+	var boxHeight = renderer.height / 20;
+	
+	var bg = new PIXI.Sprite(PIXI.Texture.fromImage('bg.png'));
+	stage.addChildAt(bg, 0);
 
-stage.addChild(bunny);
+	var bunny = new PIXI.Sprite(PIXI.Texture.fromImage('sprite.png'));
+		bunny.position.y = 65;
+	
+	stage.addChild(bunny);
+	
+	document.addEventListener('keydown', keyboard);
+	
+	(function animate() {
+		renderer.render(stage);
+		requestAnimationFrame(animate);
+	})()
+	
+	function keyboard(key) {
+		// Forward
+		if (key.keyCode === 87 || key.keyCode === 38) {
+			if (bunny.position.x != renderer.width - boxWidth) {
+				bunny.position.x += boxWidth;
+			}
+		}
+		
+		// Backward
+		if (key.keyCode === 83 || key.keyCode === 40) {
+			if (bunny.position.x != 0) {
+				bunny.position.x -= boxWidth;
+			}
+		}
+		
+		// Jump
+		if (key.keyCode === 32) {
+			if (bunny.position.y != 0) {
+				bunny.position.y -= boxHeight;
+				setTimeout(function () {
+					bunny.position.y += boxHeight;
+				}, 500);
+			}
+		}
+	}
 
-animate();
-
-function animate() {
-	requestAnimationFrame(animate);
-	renderer.render(stage);
-}
-
-document.addEventListener('keydown', onKeyDown);
-
-function onKeyDown(key) {
-
-  if (key.keyCode === 87) {
-      if (bunny.position.y != 0) {
-          bunny.position.y -= bunnyHeight;
-      }
-  }
-
-  if (key.keyCode === 83) {
-      if (bunny.position.y != renderer.height - bunnyHeight) {
-          bunny.position.y += bunnyHeight;
-      }
-  }
-
-  if (key.keyCode === 65) {
-      if (bunny.position.x != 0) {
-          bunny.position.x -= bunnyWidth;
-      }
-  }
-
-  if (key.keyCode === 68) {
-      if (bunny.position.x != renderer.width - bunnyWidth) {
-          bunny.position.x += bunnyWidth;
-      }
-  }
-  
-}
+})();
